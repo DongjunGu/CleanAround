7fee3e6e4711cab82477f2a3e6864b2838c82ffa
DongjunGu <112649273+DongjunGu@users.noreply.github.com>
2024-05-15 03:14:32 +0900

    CharacterMove&Tilemap

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public PlayerController player;
+    public static GameManager instance;
+    
+    void Awake()
+    {
+        instance = this;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 inputVector;
+    public float playerSpeed;
+
+    Rigidbody2D rigidBody;
+    SpriteRenderer spriteRenderer;
+    Animator playerAnimator;
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 moveVet = inputVector * playerSpeed * Time.fixedDeltaTime;
+        rigidBody.MovePosition(rigidBody.position + moveVet);
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVector = value.Get<Vector2>();
+    }
+    
+    void LateUpdate()
+    {
+        if(inputVector.x != 0)
+        {
+            spriteRenderer.flipX = inputVector.x > 0; //비교연산자
+        }
+
+        playerAnimator.SetFloat("playerSpeed", inputVector.magnitude);
+    }
+}
